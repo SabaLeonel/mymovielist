f7dd8a1d54e603ec42a7d7f45606ef2ef39bbc16
SabaLeonel <leonel.saba@hotmail.com>
2024-10-02 21:18:10 +0200

    testing

--- src/env.js
+++ src/env.js
@@ -6,6 +6,10 @@ export const env = createEnv({
    * Specify your server-side environment variables schema here. This way you can ensure the app
    * isn't built with invalid env vars.
    */
+
+
+
+  
   server: {
     DATABASE_URL: z.string().url(),
     NODE_ENV: z
