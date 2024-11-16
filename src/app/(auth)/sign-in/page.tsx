7b4d4094649b0cc4f4bdd61fce33f2322b820f25
sabaleonel <leonel@structo.ch>
2024-11-16 16:58:46 +0100

    adding a login/signup page

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,24 @@
+import { Suspense } from "react";
+
+function LogInPage() {
+    return (
+        <div>
+            <h1>Log In</h1>
+            <form>
+                <input type='email' placeholder='Email' />
+                <input type='password' placeholder='Password' />
+                <button type='submit'>Log In</button>
+            </form>
+        </div>
+    );
+}
+
+export default function Page() {
+    return (
+        <div className='flex justify-center items-center'>
+            <Suspense fallback={<div>Loading...</div>}>
+                <LogInPage />
+            </Suspense>
+        </div>
+    );
+}
