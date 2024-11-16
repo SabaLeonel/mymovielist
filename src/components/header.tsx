2b534f0c4e70a0eb516aabbc50300ffd2a2f7f8e
SabaLeonel <leonel.saba@hotmail.com>
2024-11-16 16:20:27 +0100

    update homepage title and remove unused links; implement session-based sign in/out in header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,10 @@
-export function Header() {
+import Link from "next/link";
+import { auth } from "~/server/auth";
+
+
+export async function Header() {
+	const session = await auth();
+
 	return (
 		<nav className="mx-auto flex h-24 max-w-7xl items-center justify-between gap-x-6 p-6 lg:px-8">
 			<div className="flex lg:flex-1">
@@ -13,7 +19,7 @@ export function Header() {
 			</div>
 			<div className="hidden flex-1 items-center justify-end gap-x-6 md:flex">
 				<div className="flex items-center gap-4">
-					<a className="line-clamp-1 flex items-center justify-center gap-2 rounded-md bg-primary px-3.5 py-2.5 text-center text-base font-bold text-white shadow-sm hover:bg-primary/80" href="/sign-in">Sign In </a>
+					<Link href={session ? "/api/auth/signout" : "/api/auth/signin"} className="line-clamp-1 flex items-center justify-center gap-2 rounded-md bg-primary px-3.5 py-2.5 text-center text-base font-bold text-white shadow-sm hover:bg-primary/80">{session ? "Sign out" : "Sign in"}</Link>
 				</div>
 			</div>
 		</nav>
