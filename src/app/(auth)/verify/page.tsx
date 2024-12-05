f9423e21781531ced076923dc21e75d6257ce1bc
sabaleonel <leonel@structo.ch>
2024-12-05 17:17:07 +0100

    adding a callback page when the email has been sent

---
+++ src/app/(auth)/verify/page.tsx
@@ -0,0 +1,18 @@
+
+export function VerifyPage() {
+  //const [showForm, setShowForm] = useState(false);
+
+  return (
+   <div>
+      
+    </div>
+  );
+}
+
+export default function Page() {
+  return (
+    <div>
+      <VerifyPage />
+    </div>
+  );
+}
