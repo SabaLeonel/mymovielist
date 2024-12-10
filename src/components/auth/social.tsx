28dd7543eaeaa08c71955f7bf316b603b596d305
SabaLeonel <leonel.saba@hotmail.com>
2024-12-10 22:30:56 +0100

    feat(auth): update Google sign-in functionality to include callback URL and remove commented code

--- src/components/auth/social.tsx
+++ src/components/auth/social.tsx
@@ -5,16 +5,16 @@ import { signIn } from "next-auth/react";
 import { FcGoogle } from "react-icons/fc";
 
 export const Social = () => {
-	const onClick = (provider: "google" | "github") => {
-		signIn(provider, {
-			callbackUrl: "/"
-		});
-	}
+	// const onClick = (provider: "google" | "github") => {
+	// 	signIn(provider, {
+	// 		callbackUrl: "/"
+	// 	});
+	// }
 
 	return (
 		<div className="flex items-center w-full gap-x-2">
 			<Button
-				onClick={() => signIn("google")}
+				onClick={() => signIn("google", { callbackUrl: "/" })}
 				className="w-full h-11 text-black bg-white dark:bg-white hover:text-black hover:bg-white/85 dark:hover:bg-white/85">
 				<FcGoogle className="w-5 h-5" />
 				Sign in with Google
