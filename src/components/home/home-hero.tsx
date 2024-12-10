9224eb7f9be599c3483d340f424d90f580e2bf29
sabaleonel <leonel@structo.ch>
2024-12-10 17:57:48 +0100

    feat(home): remove console log for cleaner code in HomeHero component

--- src/components/home/home-hero.tsx
+++ src/components/home/home-hero.tsx
@@ -25,7 +25,6 @@ export const HomeHero: React.FC<HomeHeroProps> = ({
 	count = 1,
 }) => {
 	const [mounted, setMounted] = useState(false);
-	console.log(movies)
 	const item = getFilmItemsByTitle(movies, "Gladiator II");
 	useEffect(() => {
 		setMounted(true)
