24fd0a91683c1929231b1ed20eb452c2b94b77d1
SabaLeonel <leonel.saba@hotmail.com>
2024-12-16 23:25:51 +0100

    feat(detail): refactor DetailLayout and Detail components for improved parameter handling and structure

--- src/app/(detail)/movie/[id]/layout.tsx
+++ src/app/(detail)/movie/[id]/layout.tsx
@@ -10,81 +10,83 @@ import { MediaPoster } from "@/components/media/media-poster"
 import { MediaRating } from "@/components/media/media-rating"
 
 interface DetailLayoutProps {
-  params: {
-    id: string
-  }
-  children: React.ReactNode
+	params: Promise<{
+		id: string;
+	}>;
+	children: React.ReactNode
 }
 
 export async function generateMetadata({ params }: DetailLayoutProps) {
-  const { title } = await tmdb.movie.detail({
-    id: (await params).id,
-  })
+	const { id } = await params;
+	const { title } = await tmdb.movie.detail({
+		id: id,
+	})
 
-  return {
-    title,
-  }
+	return {
+		title,
+	}
 }
 
-export default async function DetailLayout({
-  params,
-  children,
-}: DetailLayoutProps) {
-  const {
-    id,
-    title,
-    overview,
-    genres,
-    vote_average,
-    vote_count,
-    backdrop_path,
-    poster_path,
-    release_date,
-    tagline,
-    videos,
-  } = await tmdb.movie.detail<WithVideos>({
-    id:  (await params).id,
-    append: "videos",
-  })
+const DetailLayout = async ({ params, children }: DetailLayoutProps) => {
+const { id } = await params;
+const {
+	title,
+	overview,
+	genres,
+	vote_average,
+	vote_count,
+	backdrop_path,
+	poster_path,
+	tagline,
+} = await tmdb.movie.detail<WithVideos>({
+	id: id,
+	append: "videos",
+})
 
-  if (!id) return notFound()
+if (!id) return notFound()
 
-  return (
-    <MediaDetailView.Root>
-      <MediaDetailView.Backdrop>
-        <MediaBackdrop image={backdrop_path} alt={title} priority />
-      </MediaDetailView.Backdrop>
+return (
+	<MediaDetailView.Root>
+		<MediaDetailView.Backdrop>
+			<MediaBackdrop image={backdrop_path} alt={title} priority />
+		</MediaDetailView.Backdrop>
 
-      <MediaDetailView.Hero>
-        <MediaDetailView.Poster>
-          <MediaPoster image={poster_path} alt={title} size="w780" priority />
-        </MediaDetailView.Poster>
+		<MediaDetailView.Hero>
+			<MediaDetailView.Poster>
+				<MediaPoster image={poster_path} alt={title} size="w780" priority />
+			</MediaDetailView.Poster>
 
-        <div className="space-y-4">
-          <MediaDetailView.Genres>
-            <MediaRating average={vote_average} count={vote_count} />
+			<div className="space-y-4">
+				<MediaDetailView.Genres>
+					<MediaRating average={vote_average} count={vote_count} />
 
-            {genres?.map((genre) => (
-              
-                <MediaDetailView.Genre key={genre.id}>
-                  {genre.name}
-                </MediaDetailView.Genre>
-            ))}
-          </MediaDetailView.Genres>
+					{genres?.map((genre) => (
 
-          <MediaDetailView.Title>{title}</MediaDetailView.Title>
+						<MediaDetailView.Genre key={genre.id}>
+							{genre.name}
+						</MediaDetailView.Genre>
+					))}
+				</MediaDetailView.Genres>
 
-          {tagline && (
-            <MediaDetailView.Overview>
-              &quot;{tagline}&quot;
-            </MediaDetailView.Overview>
-          )}
+				<MediaDetailView.Title>{title}</MediaDetailView.Title>
 
-          <MediaDetailView.Overview
-            dangerouslySetInnerHTML={{ __html: format.content(overview) }}
-          />
-        </div>
-      </MediaDetailView.Hero>
-    </MediaDetailView.Root>
-  )
+				{tagline && (
+					<MediaDetailView.Overview>
+						&quot;{tagline}&quot;
+					</MediaDetailView.Overview>
+				)}
+
+				<MediaDetailView.Overview
+					dangerouslySetInnerHTML={{ __html: format.content(overview) }}
+				/>
+			</div>
+		</MediaDetailView.Hero>
+		<MediaDetailView.Content>
+			<div className="mt-4">{children}</div>
+		</MediaDetailView.Content>
+	</MediaDetailView.Root>
+)
 }
+
+
+export default DetailLayout;
--- src/app/(detail)/movie/[id]/page.tsx
+++ src/app/(detail)/movie/[id]/page.tsx
@@ -5,80 +5,82 @@ import { format } from "@/tmdb/utils"
 import { formatValue, joiner } from "@/lib/utils"
 
 interface DetailProps {
-  params: {
-    id: string
-  }
+	params: Promise<{
+		id: string;
+	}>;
 }
 
 export default async function Detail({ params }: DetailProps) {
-  const {
-    status,
-    release_date,
-    runtime,
-    budget,
-    revenue,
-    spoken_languages,
-    production_companies,
-    original_title,
-  } = await tmdb.movie.detail({
-    id: (await params).id,
-  })
+	const { id } = await params;
 
-  const overview = [
-    {
-      title: "Release Date",
-      value: formatValue(release_date, format.date),
-    },
-    {
-      title: "Status",
-      value: formatValue(status),
-    },
-    {
-      title: "Original Title",
-      value: formatValue(original_title),
-    },
-    {
-      title: "Runtime",
-      value: formatValue(runtime, format.runtime),
-    },
-    {
-      title: "Budget",
-      value: formatValue(budget, format.currency),
-    },
-    {
-      title: "Revenue",
-      value: formatValue(revenue, format.currency),
-    },
-    {
-      title: "Language",
-      value: joiner(spoken_languages, "english_name"),
-    },
-    {
-      title: "Production Companies",
-      value: production_companies.map(({ id, name }) => (
-        <Link
-          key={id}
-          href={`/movie/discover?with_companies=${id}`}
-          className="mr-1 border-b-2 transition hover:text-foreground"
-        >
-          {name}
-        </Link>
-      )),
-    },
-  ]
+	const {
+		status,
+		release_date,
+		runtime,
+		budget,
+		revenue,
+		spoken_languages,
+		production_companies,
+		original_title,
+	} = await tmdb.movie.detail({
+		id: id,
+	})
 
-  return (
-    <section className="space-y-4">
-      <div className="grid grid-cols-2 gap-y-12 rounded border p-6 md:grid-cols-4">
-        {overview.map((item) => (
-          <div key={item.title}>
-            <h2 className="font-medium md:text-xl">{item.title}</h2>
-            <p className="mt-2 line-clamp-2 text-sm text-muted-foreground md:text-base">
-              {item.value}
-            </p>
-          </div>
-        ))}
-      </div>
-    </section>
-  )
+	const overview = [
+		{
+			title: "Release Date",
+			value: formatValue(release_date, format.date),
+		},
+		{
+			title: "Status",
+			value: formatValue(status),
+		},
+		{
+			title: "Original Title",
+			value: formatValue(original_title),
+		},
+		{
+			title: "Runtime",
+			value: formatValue(runtime, format.runtime),
+		},
+		{
+			title: "Budget",
+			value: formatValue(budget, format.currency),
+		},
+		{
+			title: "Revenue",
+			value: formatValue(revenue, format.currency),
+		},
+		{
+			title: "Language",
+			value: joiner(spoken_languages, "english_name"),
+		},
+		{
+			title: "Production Companies",
+			value: production_companies.map(({ id, name }) => (
+				<Link
+					key={id}
+					href={`/movie/discover?with_companies=${id}`}
+					className="mr-1 border-b-2 transition hover:text-foreground"
+				>
+					{name}
+				</Link>
+			)),
+		},
+	]
+
+	return (
+		<section className="space-y-4">
+			<div className="grid grid-cols-2 gap-y-12 rounded border p-6 md:grid-cols-4">
+				{overview.map((item) => (
+					<div key={item.title}>
+						<h2 className="font-medium md:text-xl">{item.title}</h2>
+						<p className="mt-2 line-clamp-2 text-sm text-muted-foreground md:text-base">
+							{item.value}
+						</p>
+					</div>
+				))}
+			</div>
+		</section>
+	)
 }
