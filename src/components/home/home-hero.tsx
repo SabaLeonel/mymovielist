f3d8338dc451533f702393c844d3bb31c37f6b26
sabaleonel <leonel@structo.ch>
2025-02-25 16:13:22 +0100

    removed useless useEffect for better performance for homeHero

--- src/components/home/home-hero.tsx
+++ src/components/home/home-hero.tsx
@@ -1,58 +1,54 @@
-"use client";
+"use client"
 
-import { MediaBackdrop } from "@/components/media/media-backdrop";
-import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
-import { Skeleton } from "@/components/ui/skeleton";
-import Link from "next/link";
-import { useEffect, useState } from "react";
-import { FaPlay } from "react-icons/fa";
+import Link from "next/link"
+import React from "react"
+import { FaPlay } from "react-icons/fa"
 
-
+import { MediaBackdrop } from "@/components/media/media-backdrop"
+import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
 
 interface HomeHeroProps {
 	movie?: any
 	label?: string
 }
 
-
-export const HomeHero: React.FC<HomeHeroProps> = ({
-	movie,
-	label,
-}) => {
-	const [mounted, setMounted] = useState(false);
-	useEffect(() => {
-		setMounted(true)
-	}, [])
-
-	if (!mounted) return <Skeleton className="h-hero relative w-full bg-transparent" />
-
-	if (!movie)
-	{
-		return <div>No movie found</div>
+export const HomeHero: React.FC<HomeHeroProps> = ({ movie, label }) => {
+	if (!movie) {
+		return <div className="p-4">No movie found.</div>
 	}
 
 	return (
-
-		<div className="h-hero relative" key={movie?.id}>
-			<MediaBackdrop image={movie?.backdrop_path} alt={movie?.title ?? ''} />
+		<div className="h-hero animate-fadeDown relative" key={movie.id}>
+			{/* The large background image */}
+			<MediaBackdrop
+				image={movie.backdrop_path}
+				alt={movie.title}
+			/>
 
 			<div className="overlay">
-				<div className=" max-w-3xl space-y-4 p-4 pb-8 text-left md:p-14">
-					<Badge variant="outline" className="select-none">{label}</Badge>
+				<div className="max-w-3xl space-y-4 p-4 pb-8 text-left md:p-14">
+					{label && (
+						<Badge variant="outline" className="select-none">
+							{label}
+						</Badge>
+					)}
 					<h1 className="line-clamp-2 text-xl font-medium leading-tight tracking-tighter md:text-4xl">
-						{movie?.title}
+						{movie.title}
 					</h1>
 					<p className="line-clamp-3 text-sm text-white md:text-lg">
-						{movie?.overview}
+						{movie.overview}
 					</p>
-					<div className="movies-left flex flex-col  gap-4 md:flex-row">
-						<Button asChild
+					<div className="flex flex-col gap-4 md:flex-row">
+						<Button
 							size="lg"
 							variant="default"
-							className="hover:bg-primary/75 rounded-full bg-white text-black hover:text-white">
-							<Link href={`/movie/${movie?.id}`} scroll={false}>
-								<FaPlay className="size-2" />Details
+							className="hover:bg-primary/75 rounded-full bg-white text-black hover:text-white"
+							asChild
+						>
+							<Link href={`/movie/${movie.id}`} scroll={false}>
+								<FaPlay className="size-4" />
+								Details
 							</Link>
 						</Button>
 					</div>
